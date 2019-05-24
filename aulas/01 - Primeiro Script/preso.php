8b16dc34645972c61a28b8df9d773970c9435251
Arthur Porto <artfporto@gmail.com>
2019-05-24 08:59:05 -0300

    Adicionando arquivo de aulas

---
+++ aulas/01 - Primeiro Script/preso.php
@@ -0,0 +1,9 @@
+<?php
+$nome = "Mateus"; // Seu nome
+$idade = 29; // Sua idade
+
+if ( $idade>=21 )
+	echo "Cuidado $nome você já pode ser preso !";
+else
+	echo "Fica tranquilo você no máximo vai perder o internato !";
+?>
